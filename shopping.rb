15c1b1a2a484013d7f6104058c15b4f71f36236e
yuukii125 <y.kipro125@gmail.com>
2021-06-13 21:51:41 +0900

    一連の流れ実装

--- shopping.rb
+++ shopping.rb
@@ -11,3 +11,16 @@ puts "いらっしゃいませ！商品を選んでください。"
 products.each.with_index(1) do |product,i|
   puts "#{i}.#{product[:name]} #{product[:price]}円"
 end
+
+print "商品の番号を選択してください>>"
+select_product_num = gets.to_i
+
+puts "#{products[select_product_num - 1][:name]}ですね。何個購入しますか？"
+
+print "商品の個数を入力してください>>"
+buy_product_num = gets.to_i
+
+total_amount = products[select_product_num - 1][:price] * buy_product_num
+
+puts "合計金額は#{total_amount}円です"
+puts "お買い上げありがとうございました！"
